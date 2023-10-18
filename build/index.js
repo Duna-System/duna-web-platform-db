8c6c5bb52f7f0a2494c4b917783b3929ff3820ba
EliasFernandes03 <elias.neto@dunasystem.com>
2023-10-18 13:58:03 -0300

    refactor

--- build/index.js
+++ build/index.js
@@ -1,26 +1,7 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.criarColecaoComValidacao = exports.UserModel = void 0;
+exports.UserModel = exports.criarColecaoComValidacao = void 0;
 const validationDB_1 = require("./validationDB");
 Object.defineProperty(exports, "criarColecaoComValidacao", { enumerable: true, get: function () { return validationDB_1.criarColecaoComValidacao; } });
-const mongoose_1 = __importDefault(require("mongoose"));
-const UserSchema = new mongoose_1.default.Schema({
-    _id: { type: String, required: true },
-    name: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, unique: true },
-    password: { type: String, required: true },
-    actingField: { type: String, default: '' },
-    phoneNumber: { type: String, default: '' },
-    postalCode: { type: String, required: true, default: '' },
-    useObjective: { type: String, default: '' },
-    imagePath: { type: String, default: '' },
-    street: { type: String, required: true, default: '' },
-    number: { type: String, required: true, default: '' },
-    complement: { type: String, default: '' },
-});
-const UserModel = mongoose_1.default.model('User', UserSchema);
-exports.UserModel = UserModel;
+const validationApp_1 = require("./validationApp");
+Object.defineProperty(exports, "UserModel", { enumerable: true, get: function () { return validationApp_1.UserModel; } });
