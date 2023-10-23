79f6d47c2d894fea422a78778932ea931bc1f535
EliasFernandes03 <elias.neto@dunasystem.com>
2023-10-23 20:20:43 -0300

    tsc

---
+++ build/index.d.ts
@@ -0,0 +1,13 @@
+import { createUserCollectionWithValidation } from './users/validationDbUsers';
+import { UserModel } from './users/validationUserModel';
+import { IUsers } from './interfaces';
+import { connectToDatabase, checkConnectionStatus, subscribeToDatabaseEvents } from './connection';
+import { IEntityDb } from './interfaces';
+import { IProjectDb } from './interfaces';
+import { entityModel } from './entity/validationEntityModel';
+import { projectModel } from './projects/validationProjectsModel';
+import { organizationModel } from './organizations/validationOrgModel';
+import { createEntityCollectionWithValidation } from './entity/validationEntityDb';
+import { createProjectCollectionWithValidation } from './projects/validationProjectsDb';
+import { createOrganizationCollectionWithValidation } from './organizations/validationOrgDb';
+export { createUserCollectionWithValidation, connectToDatabase, checkConnectionStatus, subscribeToDatabaseEvents, createEntityCollectionWithValidation, createProjectCollectionWithValidation, createOrganizationCollectionWithValidation, UserModel, entityModel, organizationModel, IUsers, IEntityDb, IProjectDb, projectModel };
