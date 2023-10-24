5e08c08aa74939df268d645bc7024a289cbf6fa6
EliasFernandes03 <elias.neto@dunasystem.com>
2023-10-24 13:49:45 -0300

    small change

---
+++ build/userProjects/validationDbUsers.d.ts
@@ -0,0 +1,1 @@
+export declare function createUserProjectCollectionWithValidation(uri: string, databaseName: string, collection_userProject_name: string): Promise<void>;
---
+++ build/userProjects/validationUserModel.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userModelProjects = void 0;
+const mongoose_1 = require("mongoose");
+const userProjectSchema = new mongoose_1.Schema({
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+});
+exports.userModelProjects = (0, mongoose_1.model)('userProject', userProjectSchema);
