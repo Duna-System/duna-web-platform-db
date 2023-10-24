60c5aea14423cff2cbadd2acd567d1b05ed6bf7d
EliasFernandes03 <elias.neto@dunasystem.com>
2023-10-24 11:22:19 -0300

    try to fix schema problem

--- build/userProjects/validationUserModel.js
+++ build/userProjects/validationUserModel.js
@@ -2,10 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.userModelProjects = void 0;
 const mongoose_1 = require("mongoose");
-const userSchema = new mongoose_1.Schema({
+const userProjectSchema = new mongoose_1.Schema({
     _id: { type: String, required: true },
     name: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
 });
-exports.userModelProjects = (0, mongoose_1.model)('user', userSchema);
+exports.userModelProjects = (0, mongoose_1.model)('user', userProjectSchema);
