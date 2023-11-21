4f538d71bda99336b3b8ae95f64cdbd55ced70a1
EliasFernandes03 <elias.neto@dunasystem.com>
2023-11-21 10:28:41 -0300

    run format

--- src/users/validationUserModel.ts
+++ src/users/validationUserModel.ts
@@ -15,7 +15,7 @@ const UserSchema = new mongoose.Schema<IUsers>({
     street: { type: String, required: true, default: '' },
     number: { type: String, required: true, default: '' },
     complement: { type: String, default: '' },
-    emailValidade:{type:String, default:false}
+    emailValidade: { type: String, default: false },
 })
 
 const UserModel: Model<IUsers> = mongoose.model<IUsers>('user', UserSchema)
