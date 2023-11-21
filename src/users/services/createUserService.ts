98a4693e5ea24d192d05bec08a491acff98ff45c
EliasFernandes03 <elias.neto@dunasystem.com>
2023-11-21 10:58:45 -0300

    format

--- src/users/services/createUserService.ts
+++ src/users/services/createUserService.ts
@@ -26,7 +26,7 @@ export async function createUserService(user: IUsers): Promise<IUsers> {
             street: user.street,
             number: user.number,
             complement: user.complement,
-            emailValidade:false,
+            emailValidade: false,
         })
 
         const savedUser = await newUser.save()
